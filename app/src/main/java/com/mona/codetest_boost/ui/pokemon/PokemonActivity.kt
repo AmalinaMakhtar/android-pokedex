f3f79d3e803c4025d05abbb1b3bf1bf2dd3c66db
mina.makhtar <nuramalina.makhta@petronas.com.my>
2021-08-11 01:26:05 +0800

    [FIX] Inflate the layout properly

--- app/src/main/java/com/mona/codetest_boost/ui/pokemon/PokemonActivity.kt
+++ app/src/main/java/com/mona/codetest_boost/ui/pokemon/PokemonActivity.kt
@@ -1,8 +1,10 @@
 package com.mona.codetest_boost.ui.pokemon
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.mona.codetest_boost.R
+import kotlinx.android.synthetic.main.act_pokemon.*
 
 class PokemonActivity : AppCompatActivity() {
 
@@ -10,10 +12,24 @@ class PokemonActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.frg_pokemon)
+        setContentView(R.layout.act_pokemon)
 
+        initToolbar()
         pokemonId = intent.extras?.getSerializable("POKEMON_ID") as String?
 
+    }
+
+    private fun initToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
