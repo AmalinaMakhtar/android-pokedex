896fdccb39a8b3fc3bfb4658eeffb1b2c99b3566
mina.makhtar <nuramalina.makhta@petronas.com.my>
2021-08-10 21:24:09 +0800

    [TASK] Add api service for fetching all pokemons [TASK] Add model for general object

---
+++ app/src/main/java/com/mona/codetest_boost/data/api/PokemonApi.kt
@@ -0,0 +1,12 @@
+package com.mona.codetest_boost.data.api
+
+import com.mona.codetest_boost.data.models.General
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface PokemonApi {
+
+    @GET("pokemon/")
+    suspend fun getAllPokemon(): Response<General>
+
+}
