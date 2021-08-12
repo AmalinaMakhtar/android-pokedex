161c8aa10c5cd637bfc586ffcf4b1cc0c256ab17
mina.makhtar <nuramalina.makhta@petronas.com.my>
2021-08-12 18:50:21 +0800

    [FIX] Change the UI

--- app/src/main/java/com/mona/codetest_boost/ui/base/MainActivity.kt
+++ app/src/main/java/com/mona/codetest_boost/ui/base/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.mona.codetest_boost.R
 import com.mona.codetest_boost.ui.favourite.FavouriteFragment
 import com.mona.codetest_boost.ui.home.HomeFragment
+import com.mona.codetest_boost.utils.replaceFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,42 +14,14 @@ class MainActivity : AppCompatActivity() {
         private const val CONTAINER_ID = R.id.frag_container
     }
 
-    private val frgHome by lazy { HomeFragment() }
-    private val frgFavourite by lazy { FavouriteFragment() }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        bottomNavigation.setOnNavigationItemSelectedListener {
-            toggleView(it.itemId)
-            return@setOnNavigationItemSelectedListener true
-        }
-
-        setDefaultNavigation()
-        toggleView(R.id.menu_home)
-
+        addHomeFragment()
     }
 
-    private fun setDefaultNavigation() {
-        bottomNavigation.inflateMenu(R.menu.bottom_navigation)
-        supportFragmentManager.beginTransaction()
-            .add(CONTAINER_ID, frgHome)
-            .add(CONTAINER_ID, frgFavourite)
-            .commit()
-    }
-
-    private fun toggleView(menu: Int) {
-        val transaction = supportFragmentManager.beginTransaction()
-
-        when (menu) {
-            R.id.menu_home -> {
-                transaction.show(frgHome).hide(frgFavourite).commit()
-            }
-            R.id.menu_fav -> {
-                transaction.show(frgFavourite).hide(frgHome).commit()
-            }
-        }
+    private fun addHomeFragment() {
+        replaceFragment(HomeFragment(), CONTAINER_ID)
     }
 
 }
